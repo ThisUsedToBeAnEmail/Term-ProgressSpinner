4a1e65ca49f2a7e2bc46d97ce6ab2f930a3d34ad
lnation <roberta@nuvolagroup.com.au>
2020-10-16 15:59:04 +0100

    v0.04

--- t/02-multiple.pl
+++ t/02-multiple.pl
@@ -2,24 +2,12 @@ use Test::More;
 use strict;
 use warnings;
 use Term::ProgressSpinner;
-
-my $line = 1;
-
-my $ps = Term::ProgressSpinner->new(
-	terminal_height => 43
-);
-
-$ps->output->print("\e[${line};1f\e[2J");
-
+diag explain "TODO change to use a single progress spinner object.";
+my $ps = Term::ProgressSpinner->new();
 $ps->slowed(0.1);
-
-$ps->terminal_line($line++);
 my $s1 = $ps->start(20);
-$ps->terminal_line($line++);
 my $s2 = $ps->start(10);
-$ps->terminal_line($line++);
 my $s3 = $ps->start(50);
-$ps->terminal_line($line++);
 my $s4 = $ps->start(30);
  
 while (!$ps->finished) {
@@ -29,13 +17,9 @@ while (!$ps->finished) {
         $ps->advance($s4) unless $s4->finished;
 }
  
-$ps->terminal_line($line++);
 $s1 = $ps->start(20);
-$ps->terminal_line($line++);
 $s2 = $ps->start(10);
-$ps->terminal_line($line++);
 $s3 = $ps->start(50);
-$ps->terminal_line($line++);
 $s4 = $ps->start(30);
  
 while ($ps->advance()) {}
